c3747be054a430bb137c8f81f6fcbf31e0fe2989
Private-Locker Admin <admin@private-locker.com>
2018-11-29 00:06:42 -0800

    Update godmode.cmd

---
+++ godmode.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo.
+echo ===============================================
+echo       -REDD-'s WIN10 GODMODE Creator
+echo       https://www.private-locker.com
+echo ===============================================
+echo Creating Godmode Folder ^& Menu..
+echo This can be used to Escalate User Permissions on Local System.
+mkdir GodMode.{ED7BA470-8E54-465E-825C-99712043E01C} 2>NUL >NUL
+echo Finished..
+pause
